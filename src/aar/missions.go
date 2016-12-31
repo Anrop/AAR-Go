d22c843719cbc438c61f4e6f6a50868d4f02a122
Björn Dahlgren <bjorn@dahlgren.at>
2016-12-31 14:00:12 +0100

    Get list of missions for a player

--- src/aar/missions.go
+++ src/aar/missions.go
@@ -7,7 +7,36 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/jackc/pgx"
 )
+
+func outputMissionsFromRows(rows *pgx.Rows, w http.ResponseWriter) error {
+	enc := json.NewEncoder(w)
+	w.Write([]byte("["))
+
+	var first = true
+
+	for rows.Next() {
+		if first {
+			first = false
+		} else {
+			w.Write([]byte(","))
+		}
+
+		mission := Mission{}
+		err := rows.Scan(&mission.ID, &mission.CreatedAt, &mission.Length, &mission.Name, &mission.World)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading mission row from database: %v", err)
+			continue
+		}
+
+		enc.Encode(mission)
+	}
+
+	w.Write([]byte("]"))
+
+	return nil
+}
 
 func outputMissions(w http.ResponseWriter) error {
 	rows, err := DB.Query(`
@@ -40,31 +69,44 @@ func outputMissions(w http.ResponseWriter) error {
 	}
 	defer rows.Close()
 
-	enc := json.NewEncoder(w)
-	w.Write([]byte("["))
+	return outputMissionsFromRows(rows, w)
+}
 
-	var first = true
+func outputPlayerMissions(playerID string, w http.ResponseWriter) error {
+	rows, err := DB.Query(`
+		SELECT
+			missions.id,
+			missions.created_at,
+			EXTRACT(
+				epoch FROM (
+					SELECT timestamp
+					FROM events
+					WHERE events.mission_id = missions.id
+					ORDER BY timestamp DESC
+					LIMIT 1
+				) - (
+					SELECT timestamp
+					FROM events
+					WHERE events.mission_id = missions.id
+					ORDER BY timestamp ASC
+					LIMIT 1
+				)
+			)::int AS length,
+			missions.name,
+			missions.world
+		FROM events
+		INNER JOIN missions ON (missions.id = events.mission_id)
+		WHERE events.data #>> '{player, uid}' = $1
+		GROUP BY missions.id
+		ORDER BY missions.created_at DESC
+	`, playerID)
 
-	for rows.Next() {
-		if first {
-			first = false
-		} else {
-			w.Write([]byte(","))
-		}
-
-		mission := Mission{}
-		err := rows.Scan(&mission.ID, &mission.CreatedAt, &mission.Length, &mission.Name, &mission.World)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading mission row from database: %v", err)
-			continue
-		}
-
-		enc.Encode(mission)
+	if err != nil {
+		return err
 	}
+	defer rows.Close()
 
-	w.Write([]byte("]"))
-
-	return nil
+	return outputMissionsFromRows(rows, w)
 }
 
 func outputMission(missionID string, w http.ResponseWriter) error {
@@ -93,6 +135,21 @@ func MissionsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := outputMissions(w); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Fprintf(os.Stderr, "Error reading missions: %v", err)
+	}
+}
+
+func PlayerMissionsHandler(w http.ResponseWriter, r *http.Request) {
+	if newRelic != nil {
+		txn := (*newRelic).StartTransaction(r.URL.Path, w, r)
+		defer txn.End()
+	}
+
+	params := mux.Vars(r)
+	playerID := params["playerId"]
+
+	if err := outputPlayerMissions(playerID, w); err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		fmt.Fprintf(os.Stderr, "Error reading missions: %v", err)
 	}
--- src/cmd/aar/main.go
+++ src/cmd/aar/main.go
@@ -46,6 +46,7 @@ func main() {
 	r.HandleFunc("/missions/{missionId}/events", aar.EventsHandler)
 	r.HandleFunc("/missions/{missionId}/players", aar.MissionPlayersHandler)
 	r.HandleFunc("/players", aar.PlayersHandler)
+	r.HandleFunc("/players/{playerId}/missions", aar.PlayerMissionsHandler)
 
 	var handler http.Handler
 	handler = handlers.CORS()(r)
